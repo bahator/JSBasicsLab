d587475a2be1cac29bbb5998669580552240219f
lefebvre thibault <sr.bahator@gmail.com>
2016-10-04 16:48:33 +0200

    test methode 4

--- src/js_basics/index.js
+++ src/js_basics/index.js
@@ -9,25 +9,35 @@ export const isNumberEven = (i) => {
 export const getFileExtension = (str) => {
   // str will be a string, but it may not have a file extension.
   // Return the file extension (with no period) if it has one, otherwise
-    if(str.lastIndexOf('.')== -1) return false;
-    return str.split(".")[str.split(".").length-1];
+    return str.lastIndexOf('.')!= -1 ? str.substr(str.lastIndexOf('.')+1): false;
 
 };
 
 export const longestString = (arr) => {
   // arr will be an array.
   // Return the longest string in the array
+  return arr.sort(function(a,b){
+      if (a instanceof String)
+          if(b instanceof String)
+                return a-b;
+          else return 1;
+      else
+         if(b instanceof String)
+             return -1
+      return 0;});
 };
 
 export const reverseString = (str) => {
   // str will be an string
   // Return a new string who's characters are in the opposite order to str's.
+   return str.split('').reverse().join('');
 };
 
 export const isPalindrome = (str) => {
   // str will be an string
   // Return true if it is a palindrome and false otherwise.
   // It should be case insensitive and not consider space or punctuation.
+    return str.toLowerCase()==reverseString(str.toLowerCase());
 };
 
 export const nestedSum = (arr) => {
